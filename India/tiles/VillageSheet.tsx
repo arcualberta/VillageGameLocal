6fd07df1be60a51d8540cf5544902646ab5f9ddd
Mark McKellar <mpm1@ualberta.ca>
2016-03-07 12:56:26 -0700

    Added Folder "India"

---
+++ India/tiles/VillageSheet.tsx
@@ -0,0 +1,274 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="VillageSheet" tilewidth="16" tileheight="16">
+ <image source="tileset.png" width="256" height="256"/>
+ <tile id="5">
+  <animation>
+   <frame tileid="1" duration="3000"/>
+   <frame tileid="5" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="2" duration="3100"/>
+   <frame tileid="6" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="3" duration="3200"/>
+   <frame tileid="7" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="4" duration="3300"/>
+   <frame tileid="8" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="114">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="115">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="116">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="118">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="128">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="129">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="130">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="131">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="132">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="133">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="145">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="147">
+  <properties>
+   <property name="name" value="sign"/>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="148">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="149">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="161">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="162">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="163">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="176">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="177">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="178">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="192">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="193">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="194">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+</tileset>
