057097d9257e8af6f25d644e6f3d8f52e69c18a8
mpm1 <mpm1@ualberta.ca>
2018-04-07 23:11:28 -0600

    Added boat crash

--- China/maps/boat.tsx
+++ China/maps/boat.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="boat" tilewidth="70" tileheight="112" tilecount="1" columns="1">
- <image source="../tiles/boat ingame.png" width="70" height="112"/>
+<tileset name="boat" tilewidth="110" tileheight="112" tilecount="2" columns="2">
+ <image source="../tiles/boat ingame.png" width="220" height="112"/>
 </tileset>
